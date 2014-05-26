297aa7397bd628de146477403e0ac7efabfcd170
Will Jones <will@sacharissa.co.uk>
2014-05-26 21:24:17 +0100

    Begin building out the usual machinery.

--- Freeconstraining.hs
+++ Freeconstraining.hs
@@ -1,8 +1,12 @@
+{-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE UndecidableInstances #-}
@@ -69,3 +73,38 @@ addS e1 e2
 
 condS p t f
   = Mu (S (D (CondE p t f)))
+
+data Dict c a where
+  Dict :: c a => Dict c a
+
+data Proxy as
+  = Proxy
+  deriving (Eq, Show)
+
+class All c as where
+  withElem  :: (Elem b as)
+            => p as
+            -> (Dict c b -> d)
+            -> d
+
+instance All c '[] where
+  withElem _ (f :: Dict c b -> d)
+    = seq (evidence :: Evidence b '[]) (error "Impossible")
+
+instance (c a, All c as) => All c (a ': as) where
+  withElem (p :: p (a ': as)) (f :: Dict c b -> d)
+    = case evidence :: Evidence b (a ': as) of
+        Head -> f Dict
+        Tail -> withElem (undefined :: p as) f
+
+{-
+instance Functor e => Functor (ExpF e) where
+  fmap f (ValueE x)
+    = ValueE (f x)
+
+  fmap f (AddE e1 e2)
+    = AddE (fmap f e1) (fmap f e2)
+
+  fmap f (CondE pe te fe)
+    = CondE pe (fmap f te) (fmap f fe)
+    -}
